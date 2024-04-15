b61fc16bc43143167fd7938b273dcbda1a5724a3
gianlucafalaschi <gian100gian@outlook.it>
2024-04-15 15:29:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+ //import HelloWorld from './components/HelloWorld.vue'
+ export default {
+    components: {
+      
+    }
+  }
+
+</script>
+
+<template>
+ <h1>ciao</h1>
+
+</template>
+
+<style scoped>
+
+</style>
